6a38d61b8125fc9afada0894f17acf9656327889
Mark <mverleg.noreply@gmail.com>
2020-08-09 13:45:00 +0200

    Failing to reproduce file/source layout

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,35 @@
 use ::bumpalo::Bump;
-use ::std::u64;
+use ::smallvec::SmallVec;
+use ::ustr::Ustr;
 
 #[derive(Debug)]
-struct Doggo {
-    cuteness: u64,
-    age: u8,
+pub struct Element {
+    pub name: Ustr,
 }
 
-struct Park<'a> {
+#[derive(Debug)]
+pub struct Node<'a> {
+    pub elem: Element,
+    pub children: SmallVec<[&'a mut Node<'a>; 2]>
+}
+
+#[derive(Debug)]
+pub struct File<'a> {
     pub arena: Bump,
-    pub dogs: Vec<&'a mut Doggo>
-}
-
-impl <'a> Park<'a> {
-    pub fn new() -> Box<Self> {
-        Box::new(Park {
-            arena: Bump::new(),
-            dogs: vec![],
-        })
-    }
-
-    // pub fn add_dog(&mut self, dog: Doggo) -> &mut Doggo {
-    //     let allocdog = self.arena.alloc(dog);
-    //     self.dogs.push(allocdog);
-    //     allocdog
-    // }
+    pub data: Ustr,
+    pub elements: Vec<&'a mut Element>,
+    pub root: Option<Node<'a>>,
 }
 
 fn main() {
-    g(f());
-}
-
-fn f<'a>() -> Box<Park<'a>> {
-    let mut park = Park::new();
-
-    let allocdog = park.arena.alloc_with(|| Doggo {
-        cuteness: u64::max_value(),
-        age: 8,
-    });
-    park.dogs.push(allocdog);
-
-    park
-}
-
-fn g(park: Box<Park>) {
-    for dog in park.dogs {
-        println!(">> {:?}", dog);
-    }
+    let mut f1 = File {
+        arena: Bump::new(),
+        data: Ustr::from("hello world, this is some random data, don't clone it"),
+        elements: vec![],
+        root: None,
+    };
+    let mut elem: &mut Element;
+    elem = f1.arena.alloc(Element { name: Ustr::from("hello"), });
+    f1.elements.push(elem);
+    println!("{:?}", f1);
 }
